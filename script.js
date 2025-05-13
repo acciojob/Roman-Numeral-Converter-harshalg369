e5f65667d60466f5443a9889cf4215ebb3a83f93
harshalg369 <hxrsh369@gmail.com>
2025-05-13 12:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-function convertToRoman() {
-    const numberInput = document.getElementById('numberInput').value;
-    const number = parseInt(numberInput, 10);
-    const result = document.getElementById('result');
+function convertToRoman(num) {
+  const romanMap = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+  ];
 
-    if (isNaN(number) || number < 1 || number > 100000) {
-        result.textContent = "Please enter a number between 1 and 100000";
-        return;
+  let result = "";
+
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
+  }
 
-    const romanNumerals = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
-
-    let romanNumber = '';
-    let num = number;
-
-    for (let i = 0; i < romanNumerals.length; i++) {
-        while (num >= romanNumerals[i][1]) {
-            romanNumber += romanNumerals[i][0];
-            num -= romanNumerals[i][1];
-        }
-    }
-
-    result.textContent = romanNumber;
+  return result;
 }
 
-// Enable Enter key for conversion
-document.getElementById('numberInput').addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-        convertToRoman();
-    }
-});
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(convertToRoman(36));
+
+
+// do not edit below this line
+module.exports = convertToRoman;
