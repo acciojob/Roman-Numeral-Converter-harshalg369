3fcd3c4f7eefdbfaf6cc6b4d687514bae90afd02
harshalg369 <hxrsh369@gmail.com>
2025-05-13 12:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-function convertToRoman(num) {
-    const romanSymbols = [
-        ['M', 1000], 
-        ['D', 500], 
-        ['C', 100], 
-        ['L', 50], 
-        ['X', 10], 
-        ['V', 5], 
+function convertToRoman() {
+    const numberInput = document.getElementById('numberInput').value;
+    const number = parseInt(numberInput, 10);
+    const result = document.getElementById('result');
+
+    if (isNaN(number) || number < 1 || number > 100000) {
+        result.textContent = "Please enter a number between 1 and 100000";
+        return;
+    }
+
+    const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
         ['I', 1]
     ];
-    
-    let result = '';
-    
-    for (let i = 0; i < romanSymbols.length; i++) {
-        while (num >= romanSymbols[i][1]) {
-            result += romanSymbols[i][0];
-            num -= romanSymbols[i][1];
+
+    let romanNumber = '';
+    let num = number;
+
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (num >= romanNumerals[i][1]) {
+            romanNumber += romanNumerals[i][0];
+            num -= romanNumerals[i][1];
         }
     }
-    
-    return result;
+
+    result.textContent = romanNumber;
 }
+
+// Enable Enter key for conversion
+document.getElementById('numberInput').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        convertToRoman();
+    }
+});
